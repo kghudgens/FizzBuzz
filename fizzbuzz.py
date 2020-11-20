fba86634ede490ba6893f9df285233bd8b539b29
Kevin Hudgens <49671052+kghudgens@users.noreply.github.com>
2020-11-20 17:16:07 +0900

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+x= 1
+while x <=100:
+    if x % 3== 0 and x % 5 ==0:
+        print ('fizzbuzz')
+    elif x % 3==0:
+        print('fizz')
+    elif x % 5 == 0:
+        print('buzz')
+    elif x % 5 !=0 and x% 3!=0:
+        print(x)
+    x += 1
