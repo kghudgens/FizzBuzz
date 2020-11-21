0d6632e6b523e8b9b8d00bb2d5f149d6818144a4
Kevin Hudgens <49671052+kghudgens@users.noreply.github.com>
2020-11-21 12:10:25 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,4 @@
+#fizzbuzz with a while loop
 x= 1
 while x <=100:
     if x % 3== 0 and x % 5 ==0:
@@ -9,3 +10,15 @@ while x <=100:
     elif x % 5 !=0 and x% 3!=0:
         print(x)
     x += 1
+    
+  
+# execute fizzbuzz with a for loop, taking out some lines of code
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        print('fizzbuzz')
+    elif i%3==0:
+        print('fizz')
+    elif i%5==0:
+        print('buzz')
+    else:
+        print(i) 
